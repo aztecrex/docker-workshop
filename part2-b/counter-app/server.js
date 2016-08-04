06aef5bded9e3493ea70c861fdb0f8670a8a5021
Greg Wiley <gwiley@cj.com>
2016-08-04 14:25:21 -0700

    counter service

--- part2-b/counter-app/server.js
+++ part2-b/counter-app/server.js
@@ -1,15 +1,30 @@
 'use strict';
 
 const express = require('express');
-
+const redis = require('redis');
 // Constants
 const PORT = 8080;
+
+// collaborators
+var client = redis.createClient('6379', 'r0.dat');
 
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Counter\n');
+  client.get('counter', function(err, counter) {
+    if(err) return next(err);
+    res.json({'count': counter || 0})
+    // res.send('count: ' + counter);
+  });
+});
+
+app.post('/', function (req, res) {
+  client.incr('counter', function(err, counter) {
+    if(err) return next(err);
+    res.json({'count': counter || 0})
+    // res.send('count: ' + counter);
+  });
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on port ' + PORT);
